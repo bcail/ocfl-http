8ebf2b374129a6b02a465076e379db62af0f1def
Ben Cail <benjamin_cail@brown.edu>
2019-11-04 12:42:11 -0500

    add a test function for creating an OCFL repo

--- project.clj
+++ project.clj
@@ -1,10 +1,13 @@
 (defproject ocfl-http "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
+  :description "HTTP layer for OCFL"
   :url "http://example.com/FIXME"
   :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.10.0"]
                  [compojure "1.6.1"]
-                 [ring/ring-defaults "0.3.2"]]
+                 [ring/ring-defaults "0.3.2"]
+                 [edu.wisc.library.ocfl/ocfl-java-core "1.0.0-SNAPSHOT"]
+                 [edu.wisc.library.ocfl/ocfl-java-api "1.0.0-SNAPSHOT"]
+                 ]
   :plugins [[lein-ring "0.12.5"]]
   :ring {:handler ocfl-http.handler/app}
   :profiles
--- test/ocfl_http/handler_test.clj
+++ test/ocfl_http/handler_test.clj
@@ -2,6 +2,17 @@
   (:require [clojure.test :refer :all]
             [ring.mock.request :as mock]
             [ocfl-http.handler :refer :all]))
+(import '(edu.wisc.library.ocfl.core OcflRepositoryBuilder)
+        '(edu.wisc.library.ocfl.core.storage FileSystemOcflStorage)
+        '(edu.wisc.library.ocfl.core.mapping ObjectIdPathMapperBuilder))
+
+(defn get-test-repo
+  [path]
+  (let [builder (new OcflRepositoryBuilder)
+        java_path (. (clojure.java.io/as-file path) toPath)
+        mapper (. (new ObjectIdPathMapperBuilder) buildFlatMapper)
+        storage (new FileSystemOcflStorage java_path mapper)]
+    (. builder (build storage java_path))))
 
 (deftest test-app
   (testing "main route"
@@ -12,3 +23,9 @@
   (testing "not-found route"
     (let [response (app (mock/request :get "/invalid"))]
       (is (= (:status response) 404)))))
+
+(deftest test-ocfl
+  (testing "create repo"
+    (let [repo (get-test-repo "/tmp/test-ocfl")]
+      (println (. repo toString)))))
+
