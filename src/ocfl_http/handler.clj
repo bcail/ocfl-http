d40304dd45875410f0b3eef4574c3c487c1d98c4
Ben Cail <benjamin_cail@brown.edu>
2019-12-04 12:34:34 -0500

    handle PUT requests for updating files

--- src/ocfl_http/handler.clj
+++ src/ocfl_http/handler.clj
@@ -2,7 +2,7 @@
   (:require [compojure.core :refer :all]
             [compojure.route :as route]
             [ring.middleware.defaults :refer [wrap-defaults site-defaults]]
-            [ocfl-http.ocfllib :refer [get-file write-file-to-object]]))
+            [ocfl-http.ocfllib :refer [get-file write-file-to-object update-file-in-object]]))
 
 (defn ingest
   [request]
@@ -14,10 +14,21 @@
       (write-file-to-object objectId inputStream path commitInfo))
     "ingested"))
 
+(defn update-file
+  [request]
+  (do
+    (let [objectId (:objectid (:params request))
+          commitInfo {"name" "A" "address" "fake address" "message" "test message"}
+          path (:path (:params request))
+          inputStream (:body request)]
+      (update-file-in-object objectId inputStream path commitInfo))
+  "updated"))
+
 (defroutes app-routes
   (GET "/" [] "OCFL HTTP")
   (GET "/:objectid/:path" [objectid path] (get-file objectid path))
   (POST "/:objectid/:path" [] ingest)
+  (PUT "/:objectid/:path" [] update-file)
   (route/not-found "Not Found"))
 
 (def app
--- test/ocfl_http/handler_test.clj
+++ test/ocfl_http/handler_test.clj
@@ -12,7 +12,7 @@
         filePath (str contentDir "/file")]
     (do
       (spit (clojure.java.io/file filePath) "content")
-      (add-path-to-object "o1" filePath commitInfo)
+      (add-path-to-object "testsuite:1" filePath commitInfo)
       (delete-dir (str contentDir)))))
 
 ;create test app w/ security disabled for testing POST requests
@@ -31,15 +31,27 @@
       (is (= (:status response) 404)))))
 
 (deftest test-create
-  (testing "create object"
+  (testing "create object (by adding the first file)"
     (let [repoDir (create-tmp-dir)]
       (do
-        (dosync (ref-set REPO_DIR (str repoDir)))
+        (dosync (ref-set REPO_DIR repoDir))
         (let [response (test-app (-> (mock/request :post "/testsuite:1/file1")
                                      (mock/body "content")))]
           (is (= (:status response) 200))
           (is (= (get-file "testsuite:1" "file1") "content")))
         (delete-dir repoDir)))))
+
+(deftest test-update
+  (testing "update a file in an object"
+    (let [repoDir (create-tmp-dir)]
+      (do
+        (dosync (ref-set REPO_DIR repoDir))
+        (add-test-object)
+        (let [response (test-app (-> (mock/request :put "/testsuite:1/file")
+                                     (mock/body "updated contents")))]
+          (is (= (:status response) 200))
+          (is (= (get-file "testsuite:1" "file") "updated contents"))
+        (delete-dir repoDir))))))
 
 (deftest test-get-file
   (testing "get file from ocfl object"
@@ -47,7 +59,7 @@
       (do
         (dosync (ref-set REPO_DIR repoDir))
         (add-test-object)
-        (let [response (app (mock/request :get "/o1/file"))]
+        (let [response (app (mock/request :get "/testsuite:1/file"))]
           (is (= (:status response) 200))
           (is (= (:body response) "content")))
         (delete-dir repoDir)))))
